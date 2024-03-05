879aeec99a3a0a6c2557c32fc1275789e66a43e7
Syed Sajjad Askari <84753561+syedsajjadaskari@users.noreply.github.com>
2024-03-05 11:33:11 +0000

    terraform verions

---
+++ amazon-eks/versions.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_version = ">= 1.7.4"
+  required_providers {
+    aws = {
+        source = "hashicorp/aws"
+        version = ">= 5.31"
+    }
+  }
+}
+provider "aws" {
+  region = var.aws_region
+}
