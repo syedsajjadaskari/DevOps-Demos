baaa6b4bb3ce26b707a5507e0cbf07b51d99415b
Syed Sajjad Askari <84753561+syedsajjadaskari@users.noreply.github.com>
2024-03-05 11:33:23 +0000

    variables generic

---
+++ amazon-eks/generic-variables.tf
@@ -0,0 +1,12 @@
+variable "aws_region" {
+  type = string
+  default = "us-east-1"
+}
+variable "envirnment" {
+  type = string
+  default = "dev"
+}
+variable "business_division" {
+  default = "SAP"
+  type = string
+}
